2d2cc9b61d603763e07ae9596136bce0690d2d57
AmujoDotun <samuelamujo@gmail.com>
2021-01-26 13:05:08 +0100

    added raw data using backslash

--- hello.go
+++ hello.go
@@ -5,4 +5,11 @@ import "fmt"
 func  main(){
 	// this is the first program by greeting the world
 	fmt.Println("Hello World !")
+
+	// testing out back quote which is raw literal
+	a := `say "Hello" to Go!`
+	b := `Testing this again "I know" I want it on another line "\n"`
+
+	fmt.Println(a)
+	fmt.Println(b)
 }
